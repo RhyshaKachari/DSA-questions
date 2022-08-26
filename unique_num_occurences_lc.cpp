be5dcfc52593a926fe067b1f022937da391583a0
Rhysha Kachari <rhyshakachari2003@gmail.com>
2022-08-26 10:34:20 +0530

    First ques done

---
+++ unique_num_occurences_lc.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+bool uniqueOccurences(vector<int> & arr){
+    map<int ,int> mp;
+    for ( auto x : arr)
+    {
+        mp[x] ++;
+    }
+    set<int> st;
+    for ( auto it : mp)
+    {
+       if (st.count(it.second) > 0)
+       {
+        return false;
+       
+       }
+        else st.insert(it.second);
+    }
+    return true;
+}    
+    
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+  int n ;
+  cin>>n;
+    vector<int> arr;
+for (int i = 0; i < n; i++)
+{int x;
+ cin>>x;
+ arr.push_back(x);
+}
+
+cout<<uniqueOccurences(arr);
+
+}
