5f1df6fbbf83a8661a4b18d36f607b79cc007870
Rhysha Kachari <rhyshakachari2003@gmail.com>
2022-09-24 17:29:35 +0530

    Bit Manipulation

---
+++ question13.cpp
@@ -0,0 +1,18 @@
+//Number of 1 Bits - Easy
+//Leetcode
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+int count = 0;
+while(n!=0){
+if(n%2==1)
+{
+count ++;
+}
+n= n>>1;
+}
+return count;
+
+    }
+    
+};
