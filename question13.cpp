c3171332f98a2d34fdc108a7889c2adec224930b
Rhysha Kachari <rhyshakachari2003@gmail.com>
2022-10-07 16:41:21 +0530

    Number of 1 Bits- Leetcode

--- question13.cpp
+++ question13.cpp
@@ -1,19 +1,32 @@
 //Number of 1 Bits - Easy
 //Leetcode 
-class Solution {
-public:
-   
-int hammingWeight(uint32_t n) {
-int count = 0;
-while(n!=0){
-if(n%2==1)
-{
-count ++;
-}
-n= n>>1;
-}
-return count;
 
-    }
+// class Solution {
+// public:
+   
+// int hammingWeight(uint32_t n) {
+// int count = 0;
+// while(n!=0){
+// if(n%2==1)
+// {
+// count ++;
+// }
+// n= n>>1;
+// }
+// return count;
+
+//     }
     
-};
+// };
+
+//More Optimised
+class Solution{
+public:
+int hammingWeight(uint32_5 n){
+int count = 0;
+while(n){
+n = (n&(n-1));
+count ++ ;}
+return count;
+}};
+
