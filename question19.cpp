1214f777e429b9918f0f6fe9a8938554627bbf78
Rhysha Kachari <rhyshakachari2003@gmail.com>
2022-10-07 17:47:38 +0530

    Subsets

---
+++ question19.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    vector<vector<int>> subsets(vector<int> &nums){
+        int n =  nums.size();
+        vector<vector<int>> subs;
+        for(int i =0 ; i<(1<<n) ; i++){
+            vector<int> arr;
+            for (int j = 0; j <n; j++)
+            {
+              if(i&(1<<j)){
+                arr.push_back(nums[j]);
+
+              }
+            }
+         subs.push_back(arr);   
+        }
+        return subs;
+
+    }
+};
