457c02efb17187d7956e5b6c5cc70bc9a94beefa
Rhysha Kachari <rhyshakachari2003@gmail.com>
2022-10-07 18:11:57 +0530

    Single Number - Leetcode

---
+++ question20.cpp
@@ -0,0 +1,13 @@
+//Single Number - Leetcode
+//Easy
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+ int ans = 0;
+for(int i = 0 ; i<nums.size() ; i++){
+ans = ans^nums[i];}
+return ans;
+
+
+    }
+};
