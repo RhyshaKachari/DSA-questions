ff84293ac1bb03b0058215057a8da4063a44c513
Rhysha Kachari <rhyshakachari2003@gmail.com>
2022-12-29 17:29:04 +0530

    Zig Zag Tree Traversal - GFG

--- question105.cpp
+++ question105.cpp
@@ -46,3 +46,53 @@ public:
         return ans;
     }
 };
+
+// ZigZag Tree Traversal -GFG
+// Time Complexity =O(N)
+// Space Complexity = O(N)
+
+class Solution
+{
+public:
+    vector<int> zigZagTraversal(Node *root)
+    {
+        vector<int> ans;
+        int flag = 0;
+        queue<Node *> q;
+        if (root == NULL)
+        {
+            return ans;
+        }
+        q.push(root);
+        while (!q.empty())
+        {
+            vector<int> v;
+            int len = q.size();
+            for (int i = 0; i < len; i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                v.push_back(node->data);
+                if (node->left != NULL)
+                {
+                    q.push(node->left);
+                }
+                if (node->right != NULL)
+                {
+                    q.push(node->right);
+                }
+            }
+            if (flag)
+            {
+                reverse(v.begin(), v.end());
+              
+            }
+            for (auto it : v)
+            {
+                ans.push_back(it);
+            }  flag = !flag;
+        }
+
+        return ans;
+    }
+};
