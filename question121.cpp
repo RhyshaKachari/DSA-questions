30fa0a20da57ee03c41c5410a32637e37c0fbe7d
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-01-04 16:25:08 +0530

    01 Matrix - Leetcode

---
+++ question121.cpp
@@ -0,0 +1,55 @@
+// 01 Matrix - Leetcode
+// Time Complexity = O(N*M)
+// Space Complexity = O(N*M)
+
+class Solution
+{
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> dis(n, vector<int>(m, 0));
+        queue<pair<pair<int, int>, int>> q;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (mat[i][j] == 0)
+                {
+                    vis[i][j] = 1;
+                    q.push({{i, j}, 0});
+                }
+                else
+                {
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int delRow[] = {-1, 0, 0, 1};
+        int delCol[] = {0, -1, 1, 0};
+
+        while (!q.empty())
+        {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int step = q.front().second;
+            dis[row][col] = step;
+            q.pop();
+            for (int i = 0; i < 4; i++)
+            {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0)
+                {
+                    q.push({{nrow, ncol}, step + 1});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+
+        return dis;
+    }
+};
