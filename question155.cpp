61268e6ad0001f2dc6ac6c163cc71cab0c636473
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-01-13 19:11:34 +0530

    Rotate Image - Leetcode

---
+++ question155.cpp
@@ -0,0 +1,47 @@
+// Rotate Image - Leetcode
+
+// Approach 1 : Brute Force
+// Time Complexity = O(N^2)
+// Space Complexity = O(N^2)
+
+class Solution
+{
+public:
+    vector<vector<int>> rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        vector<vector<int>> rotated(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+};
+
+// Approach 2 : Optimal Approach
+// Time Complexity = O(N^2)
+// Space Complexity = O(1)
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
