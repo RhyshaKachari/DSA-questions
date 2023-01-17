0e83a840f00b7e3575db343b0e3d54fdc36c868f
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-01-17 19:37:30 +0530

    Find Smallest Letter Greater Than Target - Leetcode

--- question168.cpp
+++ question168.cpp
@@ -1,29 +1,45 @@
-//Find Smallest Letter Greater Than Target - Leetcode
-//Time Complexity = O(log n)
-//Space Complexity = O(1)
+// Find Smallest Letter Greater Than Target - Leetcode
+// Time Complexity = O(log n)
+// Space Complexity = O(1)
 
-class Solution{
+class Solution
+{
 public:
-char nextGreatestLetter(vector<char> &letters , char target){
-   int low = 0;
-   int high = letters.size() -1;
-   if(target < letters[0] || target > letters[letters.size() -1]){
-    return letters[0];
-   }
-   int res = -1 ;
-   while(low <= high){
-    int mid = low + (high-low)/2 ;
-    if(letters[mid] == target){
-        low = mid+1; 
+    char nextGreatestLetter(vector<char> &letters, char target)
+    {
+        int low = 0;
+        int high = letters.size() - 1;
+        if (target < letters[0] || target > letters[letters.size() - 1])
+        {
+            return letters[0];
+        }
+        int res = 0;
+        while (low <= high)
+        {
+
+            int mid = low + (high - low) / 2;
+            if (low == high)
+            {
+                if (letters[low] > target)
+                {
+                    res = low;
+                    break;
+                }
+            }
+            if (letters[mid] == target)
+            {
+                low = mid + 1;
+            }
+            else if (letters[mid] > target)
+            {
+                res = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return letters[res];
     }
-    else if(letters[mid] > target){
-        res = mid;
-        high = mid-1;
-    }
-    else{
-        low = mid+1;
-}
-   }
-   return letters[res];
-}
 };
