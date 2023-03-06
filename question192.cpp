70871d835ad16f5ff4962d9f99f339a9c11acd7c
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-03-06 05:59:08 +0530

    Count Occurences of Anagrams - GFG

---
+++ question192.cpp
@@ -0,0 +1,67 @@
+// Count Occurences of Anagrams - GFG
+
+// Time Complexity : O(n)
+// Space Complexity : O(26) or O(256)
+
+class Solution
+{
+public:
+    int search(string pat, string txt)
+    {
+        int ans = 0;
+        unordered_map<char, int> mp;
+        int i = 0;
+        int j = 0;
+        int n = txt.size();
+
+        // counting all the characters in the string pat
+        for (auto c : pat)
+        {
+            mp[c]++;
+        }
+        int count = mp.size();
+        int k = pat.size();
+        while (j < n)
+        {
+            // calculation part
+            if (mp.find(txt[j]) != mp.end())
+            {
+                mp[txt[j]]--;
+                if (mp[txt[j]] == 0)
+                {
+                    count--;
+                }
+            }
+
+            // window length is not achieved yet
+            if (j - i + 1 < k)
+            {
+                j++;
+            }
+
+            // window length is achieved , find ans , slide the window
+            else if (j - i + 1 == k)
+            {
+                // finding ans
+                if (count == 0)
+                {
+                    ans++;
+                }
+
+                // sliding the window
+                if (mp.find(txt[i]) != mp.end())
+                {
+                    mp[txt[i]]++;
+                    if (mp[txt[i]] == 1)
+                    {
+                        count++;
+                    }
+                }
+
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
