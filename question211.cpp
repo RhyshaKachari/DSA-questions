31d0d87904041fcbd62613306e6909579bcab579
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-03-12 18:25:59 +0530

    Reverse Node in K Group -  Leetcode

---
+++ question211.cpp
@@ -0,0 +1,54 @@
+// Reverse Nodes in K Group - Leetcode
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+class Solution
+{
+public:
+    ListNode *solve(ListNode *head, int k)
+    {
+
+        // base condition
+        if (head == NULL)
+        {
+            return head;
+        }
+        // reverse first k nodes
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        ListNode *fwd = NULL;
+        ListNode *check = curr;
+        int t = 0;
+        while (t < k && check != NULL)
+        {
+            check = check->next;
+            t++;
+        }
+        if (t < k)
+        {
+            return curr;
+        }
+        int cnt = 0;
+        while (curr != NULL && cnt < k)
+        {
+            fwd = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fwd;
+            cnt++;
+        }
+
+        // Recursion
+        if (fwd != NULL)
+        {
+            head->next = solve(fwd, k);
+        }
+        // return head of reversed list
+        return prev;
+    }
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        return solve(head, k);
+    }
+};
