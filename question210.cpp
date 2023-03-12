226507f0cc18f590071a30b6c93f4c8217833d65
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-03-12 16:08:20 +0530

    Finding middle element in a linked list - GFG

--- question210.cpp
+++ question210.cpp
@@ -1,5 +1,6 @@
 // Finding middle element in a linked list - GFG
 
+//Approach 1 : Finding the length first
 // Time Complexity : O(n)
 // Space Complexity : O(1)
 
@@ -24,5 +25,27 @@ public:
             cnt++;
         }
         return temp->data;
+    }
+};
+
+
+//Approach 2 : Tortoise Hare Method
+
+//Time Complexity = O(n)
+//Space Complexity = O(1)
+
+class Solution{
+    public:
+    int getMiddle(Node *head){
+    Node* slow = head;
+    Node* fast = head->next;
+    while(fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            fast = fast->next;
+        }
+        slow = slow->next;
+    }
+    return slow->data;
     }
 };
