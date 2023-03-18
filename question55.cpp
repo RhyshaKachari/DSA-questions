fc28e5f2aee1f916b8bb9376bf1354e5e5a3d4d4
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-03-18 09:17:48 +0530

    Subset Sums - GFG

---
+++ question55.cpp
@@ -0,0 +1,25 @@
+//Subset Sums - GFG
+
+//Time Complexity = O(2^n)
+//Space Complexity = O(2^n)
+
+class Solution{
+    public:
+    void solve(int ind , int &sum , int n ,vector<int>&ans, vector<int>&arr){
+     if(ind == n){
+        ans.push_back(sum);
+        return;
+     }   
+
+     //take condition
+     solve(ind+1 , sum+arr[ind] , n , ans, arr);
+     //not take condition
+     solve(ind+1 , sum , n , ans , arr);
+    }
+    vector<int> subsetSums(vector<int>arr ,int N){
+        vector<int> ans;
+        solve(0 , 0 , N , ans , arr);
+        sort(ans.begin() , ans.end());
+        return ans;
+    }
+}
