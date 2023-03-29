1f5cf0a21689ea63892894b1e95407a900f8192a
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-03-30 05:16:55 +0530

    Find Peak Element - Leetcode

---
+++ question251.cpp
@@ -0,0 +1,32 @@
+//Find Peak Element - Leetcode
+//Time Complexity = O(log n)
+//Space Complexity = O(1)
+
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1]))
+            {
+                return mid;
+            }
+
+            else if (mid > 0 && nums[mid - 1] > nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
