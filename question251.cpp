36ee072f830fcc9039bbb74234f9975a26e9ef06
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-03-30 05:21:00 +0530

    Find Peak Element - Leetcode

--- question251.cpp
+++ question251.cpp
@@ -13,7 +13,7 @@ public:
         while (low <= high)
         {
             int mid = low + (high - low) / 2;
-            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1]))
+            if ((mid == 0 || nums[mid] >= nums[mid - 1]) && (mid == n - 1 || nums[mid] >= nums[mid + 1]))
             {
                 return mid;
             }
