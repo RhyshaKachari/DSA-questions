50d2a01a379c1520a234e48a0c810d2c780d6053
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-04-01 00:50:18 +0530

    Largest Odd Number in String - Leetcode

---
+++ question257.cpp
@@ -0,0 +1,22 @@
+// Largest Odd Number in String - Leetcode
+
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+
+class Solution
+{
+public:
+    string largestOddNumber(string num)
+    {
+        int n = num.length();
+        int r;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if ((num[i] - '0') & 1)
+            {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
