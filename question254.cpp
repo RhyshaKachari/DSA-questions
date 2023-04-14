2109b36a21fd53c77775da4a1b017d024e299400
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-04-15 04:42:45 +0530

    Reverse words in a given string - GFG

--- question254.cpp
+++ question254.cpp
@@ -30,3 +30,34 @@ public:
         return ans;
     }
 }
+
+// Approach 2 : We will start traversing the string from the back and keep storing it in a temp variable until we find a dot and once we find a dot then we simply reverse the temp variable and add it in the answer
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+
+class Solution
+{
+public:
+    string reverseWords(string S)
+    {
+        string ans = "";
+        string temp = "";
+        for (int i = S.length() - 1; i >= 0; i--)
+        {
+            if (S[i] == '.')
+            {
+                reverse(temp.begin(), temp.end());
+                ans = ans + temp;
+                ans.push_back('.');
+                temp = "";
+            }
+            else
+            {
+                temp.push_back(S[i]);
+            }
+        }
+        reverse(temp.begin(), temp.end());
+        ans = ans + temp;
+        return ans;
+    }
+};
