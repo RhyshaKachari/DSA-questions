bad85a6b0527071bef9ccb455c735460ae121033
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-08 13:10:20 +0530

    Longest Common Prefix in an Array - GFG

--- question300.cpp
+++ question300.cpp
@@ -1,8 +1,7 @@
 // Longest Common Prefix - Leetcode
 
 // Time Complexity = O(N* mini)
-// Space Complexity = O(1)
-
+// Auxiliary Space: O(min(|arri|))
 class Solution
 {
 public:
@@ -33,6 +32,52 @@ public:
             }
             count++;
         }
+        return str.substr(0, count);
+    }
+};
+
+// Longest Common Prefix in an Array - GFG
+
+// Time Complexity: O(N*min(|arri|))
+// Auxiliary Space: O(min(|arri|))
+
+class Solution
+{
+public:
+    string longestCommonPrefix(string arr[], int N)
+    {
+        int mini = 1000;
+        string str;
+        for (int i = 0; i < N; i++)
+        {
+            int len = arr[i].length();
+            if (len < mini)
+            {
+                str = arr[i];
+                mini = len;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < mini; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (arr[j][i] != str[i])
+                {
+                    if (count == 0)
+                    {
+                        return "-1";
+                    }
+                    else
+                    {
+                        return str.substr(0, count);
+                    }
+                }
+            }
+            count++;
+        }
+
         return str.substr(0, count);
     }
 };
