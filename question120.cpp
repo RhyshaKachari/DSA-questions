e5b13f3198684197468487303345890e6b13fc2e
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-24 12:27:14 +0530

    Detect cycle in an undirected graph using BFS and DFS

--- question120.cpp
+++ question120.cpp
@@ -49,3 +49,40 @@ public:
         return false;
     }
 };
+
+
+//Approach 2 : DFS
+//Time Complexity  : O(V+2E) 
+//Space Complexity : O(V) +O(V) recursive stack space
+
+class Solution {
+  private:
+  bool dfs(int node , int parent , vector<int>adj[] , int vis[]){
+      vis[node] = 1;
+      for(auto it : adj[node]){
+          if(!vis[it]){
+              if(dfs(it , node, adj , vis) == true){
+                  return true;
+              }
+          }
+          else if(parent != it){
+              return true ;
+          }
+      }
+      return false;
+  }
+  public:
+    
+    bool isCycle(int V, vector<int> adj[]) {
+       int vis[V] = {0} ;
+       for(int i =  0 ; i < V ; i++){
+           if(!vis[i]){
+                if(dfs(i , -1 , adj , vis) == true){
+               return true ;
+           }
+           }
+          
+       }
+       return false;
+    }
+};
