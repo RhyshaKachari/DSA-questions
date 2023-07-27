bd983d4a08fb9ac3b399ce1aaf6422aba0005487
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-27 12:40:10 +0530

    Fractional Knapsack - GFG

--- question273.cpp
+++ question273.cpp
@@ -1,8 +1,7 @@
 // Fractional Knapsack - GFG
 
 // Time Complexity = O(n log n)
-// Space Complexity = O(1)
-
+// Space Complexity = O(n)
 
 class Solution
 {
@@ -41,3 +40,41 @@ public:
         return totalValue;
     }
 }
+
+// Approach 2 : Without using any extra space
+// Time Complexity - O(N log N)
+// Space Complexity - O(1)
+
+
+class Solution
+{
+public:
+    static bool comp(Item a, Item b)
+    {
+        double r1 = (double)a.value / (double)a.weight;
+        double r2 = (double)b.value / (double)b.weight;
+        return r1 > r2;
+    }
+
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        sort(arr, arr + n, comp);
+        int currWeight = 0;
+        double finalValue = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            if (currWeight + arr[i].weight <= W)
+            {
+                currWeight += arr[i].weight;
+                finalValue += arr[i].value;
+            }
+            else
+            {
+                int remain = W - currWeight;
+                finalValue += (arr[i].value / (double)arr[i].weight) * (double)remain;
+                break;
+            }
+        }
+        return finalValue;
+    }
+};
