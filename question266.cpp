805a751d71a0842361ce405c16e9432cdc359d24
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-28 16:09:45 +0530

    N meetings in one room - GFG

--- question266.cpp
+++ question266.cpp
@@ -2,33 +2,45 @@
 
 // Approach 1 : Greedy Approach - We will sort all the meetings in increasing order of their end times. A meeting can be held if the start time of it is greater than the end time of the previous meeting
 
+//Time Complexity - O(N log N)
+//Space Complexity - O(N)
+
 class Solution
 {
-public:
-    static bool cmp(pair<int, int> a, pair<int, int> b)
+    struct meeting
     {
-        return a.second < b.second;
+        int start ; 
+        int end ; 
+        int pos; 
+    };
+    
+    static bool cmp(struct meeting m1 ,struct meeting m2){
+        if(m1.end < m2.end){return true ;}
+        else if(m1.end > m2.end){return false ;}
+        else if(m1.pos < m2.pos){return true ;}
+        return false; 
     }
-
+    public:
+    
     int maxMeetings(int start[], int end[], int n)
-    {
-        vector<pair<int, int>> v;
-        for (int i = 0; i < n; i++)
-        {
-            pair<int, int> p = make_pair(start[i], end[i]);
-            v.push_back(p);
+    {   struct meeting meet[n] ;
+        for(int i = 0 ; i < n ; i++){
+            meet[i].start = start[i] ;
+            meet[i].end = end[i] ;
+            meet[i].pos = i ;
         }
-        sort(v.begin(), v.end(), cmp);
-        int cnt = 1;
-        int endTime = v[0].second;
-        for (int i = 1; i < n; i++)
-        {
-            if (v[i].first > endTime)
-            {
-                cnt++;
-                endTime = v[i].second;
+
+        sort(meet , meet+n , cmp) ;
+        vector<int>meets ;
+        int limit = meet[0].end ; 
+        meets.push_back(meet[0].pos) ; 
+
+        for(int i  = 0  ; i < n ; i++){
+            if(meet[i].start > limit){
+                limit = meet[i].end ;
+                meets.push_back(meet[i].pos) ;
             }
         }
-        return cnt;
+        return meets.size(); 
     }
-}
+};
