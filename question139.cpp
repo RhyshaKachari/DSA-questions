49c580c9eb4f019e2abd740a1b4fcaad139d8aa3
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-30 12:10:07 +0530

    JUMP GAME II - LEETCODE

--- question139.cpp
+++ question139.cpp
@@ -1,5 +1,5 @@
 // JUMP GAME II - LEETCODE
-// Time Complexity = O(N)
+// Time Complexity = O(N*1000) or O(N^2)
 // Space Complexity = O(N)
 
 class Solution
@@ -8,7 +8,7 @@ private:
     int solve(int ind, int n, vector<int> &nums, vector<int> &dp)
     {
         int ans = 1e8;
-        if (ind >= n -1)
+        if (ind >= n - 1)
         {
             return 0;
         }
@@ -29,5 +29,33 @@ public:
         int n = nums.size();
         vector<int> dp(n + 1, -1);
         return solve(0, n, nums, dp);
+    }
+};
+
+// Approach 2 : Greedy Approach
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+        int n = nums.size();
+        int start = 0;
+        int end = 0;
+        int farthest = 0;
+        int jump = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == end)
+            {
+                jump++;
+                end = farthest;
+            }
+        }
+        return jump;
     }
 };
