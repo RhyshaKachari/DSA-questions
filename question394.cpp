418e6b68289f8d9843274c213c7ac3c832157486
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-07-31 20:37:07 +0530

    Array Partition - Leetcode

---
+++ question394.cpp
@@ -0,0 +1,23 @@
+// Array Partition - Leetcode
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+class Solution
+{
+public:
+    int arrayPairSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                ans += nums[i];
+            }
+        }
+        return ans;
+    }
+};
