da56bb05a2b01673cb9c159b306093ed856e00d5
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-08-16 14:19:07 +0530

    Kth smallest element - GFG

--- question172.cpp
+++ question172.cpp
@@ -19,3 +19,64 @@ public:
         return maxH.top();
     }
 };
+
+// Approach 2 : Using quick selection method
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+class Solution
+{
+public:
+    int partition(vector<int> &arr, int left, int right)
+    {
+        int pivot = arr[left];
+        int l = left + 1;
+        int r = right;
+        while (l <= r)
+        {
+            if (arr[l] < pivot && arr[r] > pivot)
+            {
+                swap(arr[r], arr[l]);
+                l++;
+                r--;
+            }
+            else if (arr[l] >= pivot)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        swap(arr[left], arr[r]);
+        return r;
+    }
+
+    int findKthsmallest(vector<int> &arr, int k)
+    {
+        int n = arr.size();
+        int z = n - k + 1;
+        int left = 0;
+        int right = n - 1;
+        int zth;
+        while (1)
+        {
+            int index = partition(arr, left, right);
+            if (index == z - 1)
+            {
+                kth = arr[index];
+                break;
+            }
+            else if (index < z - 1)
+            {
+                left = index + 1;
+            }
+            else
+            {
+                right = index - 1;
+            }
+        }
+        return zth;
+    }
+}
