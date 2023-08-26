85e8b048620d19aef8f1cb67e6da4051068ec055
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-08-26 10:14:42 +0530

    First and Last Occurrences of x - GFG

--- question159.cpp
+++ question159.cpp
@@ -61,3 +61,64 @@ vector<int> find(int arr[], int n, int x)
     ans.push_back(last);
     return ans;
 }
+
+// Approach 2 : Lower Bound and Upper Bound Concept
+// Time Complexity - O(log n)
+// Space Complexity - O(1)
+
+#include <bits/stdc++.h>
+
+int lower_bound(vector<int> &arr, int n, int k)
+{
+    int low = 0;
+    int high = n - 1;
+    int ans = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] >= k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int upper_bound(vector<int> &arr, int n, int k)
+{
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] > k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+    int first = lower_bound(arr, n, k);
+    if (first == -1 || arr[first] != k)
+    {
+        return {-1, -1};
+    }
+    else
+    {
+        return {first, upper_bound(arr, n, k) - 1};
+    }
+}
